844a66f93c67999e9dfd33dbb3712b5bd71e442b
GytAndr <andriejunas@gmail.com>
2022-06-27 17:24:05 +0300

    final commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let app = express();
+
+app.listen(3000);
+
+app.get('/', (req, res) => {
+	res.sendFile('./index.html', { root: __dirname });
+});
+app.get('/about', (req, res) => {
+	res.sendFile('./about.html', { root: __dirname });
+});
+app.get('/contacts', (req, res) => {
+	res.sendFile('./contact-me.html', { root: __dirname });
+});
